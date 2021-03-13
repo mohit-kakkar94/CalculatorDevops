712148ef70c27c9b3a2886571244299eb704f67b
Mohit Kakkar <k.mohit.1694@gmail.com>
2021-03-13 22:43:21 +0530

    Added functionality and testing features - Updated

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,152 +1,175 @@
-import java.util.Scanner;
+package calculator;
+import java.util.*;
+//line added
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import java.lang.Math;
+//line added
+public class App {
+    //line added
+    private static final Logger logger = LogManager.getLogger(App.class);
 
-public class Calculator {
+    //line added
+    //
+    public static void main(String args[]) {
+        int n;
+        //double num1,num2;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("=========================================================================================");
+        System.out.println("                                 Scientific Calculator                                   ");
+        System.out.println("=========================================================================================");
+        while (true) {
+            System.out.println(" ");
+            System.out.println(" ");
+            System.out.println("Enter 1,for Square Root\nEnter 2,for Factorial\nEnter 3,for Log base e\nEnter 4,for Power Function\nEnter 5,for EXit");
+            System.out.println(" ");
+            System.out.print("Enter your choice: ");
+            n = scan.nextInt();
 
-    private static final Logger logger = LogManager.getLogger(Calculator.class);
 
-    public static void  printAns(double ans){
-        System.out.println("************************************************");
-        System.out.println("Your answer is " + ans);
-        System.out.println("************************************************");
-    }
+            if (n == 1) {
+                System.out.println("Enter number for Square Root:");
 
-    public static double squareRoot(double num){
-        double ans = 0;
-        try {
-            logger.info("Let's calculate square root of " + num);
-            if(num < 0){
-                ans = Double.NaN;
-                throw new ArithmeticException("Given Number is negative so can't find Square Root.");
-            }else {
-                ans = Math.sqrt(num);
+                double a = scan.nextDouble();
+                double res = Sqeroot(a);
+                System.out.println("The Square root of " + a + " is " + res);
+
+            } else if (n == 2) {
+                System.out.println("Enter number for Factorial:");
+
+                Long b = scan.nextLong();
+                double res = Fact(b);
+                if(res>0)
+                {
+                    System.out.println("The Factorial of " + b + " is " + res);
+                }
+
+            } else if (n == 3) {
+                System.out.println("Enter number for Log:");
+
+                double a = scan.nextDouble();
+                double res = Loge(a);
+                System.out.println("The Log of " + a + " is " + res);
+
+
+            } else if (n == 4) {
+                System.out.println("Enter first number :");
+
+                double a = scan.nextDouble();
+                System.out.println("Enter second number :");
+
+                double b = scan.nextDouble();
+
+                double res = Powe(a,b);
+                System.out.println("Power of " + a + "and "+b+" is " + res);
+            } else if (n == 5) {
+                System.out.println("Ending the Program");
+                logger.info("Entered 5 to exit.");
+                return ;
+
+            } else {
+                System.out.println("PLease Enter Valid Option");
             }
-        }catch (ArithmeticException e){
-            logger.error("Number should not be negative : " + e.getMessage());
-        }finally {
-            logger.info("Result : " + ans);
-        }
-        return ans;
-    }
-
-    public static double factorial(double num){
-        double ans = 1;
-        try {
-            logger.info("Let's calculate factorial of " + num);
-            if(num < 0){
-                ans = Double.NaN;
-                throw new ArithmeticException("Given Number is negative so can't find factorial.");
+            /*
+            if (choice > 4 || choice <1) {
+                flag = 1;
+                System.out.println("Incorrect Option Choosed, Ending the Program\n");
             }
-            if(num != (int)num){
-                ans = Double.NaN;
-                throw new ArithmeticException("Given Number is factional so can't find factorial.");
-            }
-            else {
-                for(int i=1; i<=num; i++){
-                    ans = ans * i;
+            else if (flag != 1){
+            	 System.out.println("Enter two numbers");
+                 System.out.print("Enter number 1: ");
+                 num1 = scan.nextDouble();
+                 System.out.print("Enter number 2: ");
+                 num2 = scan.nextDouble();
+                 
+                 switch(choice) {
+                 case 1: System.out.println(num1+" + "+num2+" = "+Add(num1, num2));
+                 		break;
+                 case 2: System.out.println(num1+" - "+num2+" = "+Subtract(num1, num2));
+                 		break;
+                 case 3: System.out.println(num1+" * "+num2+" = "+Multiply(num1, num2));
+                 		break;
+                 case 4: System.out.println(num1+" / "+num2+" = "+Divide(num1, num2));
+                		break;
+                default: System.out.println("Exiting, Bye");
+                flag=1;
                 }
             }
-        }catch (ArithmeticException e){
-            logger.error("Error : " + e.getMessage());
-        }finally {
-            logger.info("Result : " + ans);
+            System.out.println("\n");
+            }while(flag==0);*/
         }
-        return ans;
+
     }
 
-    public static double naturalLog(double num){
-        double ans = 0;
-        try {
-            logger.info("Let's calculate natural log of " + num);
-            if(num <= 0){
-                ans = Double.NaN;
-                throw new ArithmeticException("Number should be > 0");
-            }
-            else {
-                ans = Math.log(num);
-            }
-        }catch (ArithmeticException e){
-            logger.error("Number should be positive only : " + e.getMessage());
-        }finally {
-            logger.info("Result : " + ans);
-        }
-        return ans;
+    //logger.error("print an error");
+    //logger.info("print some info");
+    /*static double (double a) {
+    logger.info("Adding two numbers"+a +"and");
+
+    	return a ;
     }
-
-    public static double mypower(double num1, double num2){
-        double ans = Double.NaN;
-        try {
-            logger.info("Let's calculate " + num1 + "to the power " + num2);
-            ans = Math.pow(num1, num2);
-        }catch (ArithmeticException e){
-            logger.error("Error : " + e.getMessage());
-        }finally {
-            logger.info("Result : " + ans);
-        }
-        return ans;
+    static double Subtract(double a, double b) {
+    	return a - b;
     }
-
-
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        int choice;
-        double ans, num;
-
-        do {
-
-            System.out.println("\n\nWelcome to Mini Calculator... \nChoose one of the Following operation.");
-            System.out.println("Press 1 for Square Root");
-            System.out.println("Press 2 for Factorial");
-            System.out.println("Press 3 for Natural logarithm");
-            System.out.println("Press 4 for Power function");
-            System.out.println("Press 0 to Exit");
-
-            choice = sc.nextInt();
-
-            switch (choice) {
-                case 1:
-                    System.out.println("Enter the Number");
-                    num = sc.nextDouble();
-                    ans = squareRoot(num);
-                    printAns(ans);
-                    break;
-
-                case 2:
-                    System.out.println("Enter the Number");
-                    num = sc.nextDouble();
-                    ans = factorial(num);
-                    printAns(ans);
-                    break;
-
-                case 3:
-                    System.out.println("Enter the Number");
-                    num = sc.nextDouble();
-                    ans = naturalLog(num);
-                    printAns(ans);
-                    break;
-
-                case 4:
-                    System.out.println("Enter the base number");
-                    num = sc.nextDouble();
-                    System.out.println("Enter the power number");
-                    double power = sc.nextDouble();
-                    ans = mypower(num, power);
-                    printAns(ans);
-                    break;
-
-                case 0:
-                    break;
-
-                default:
-                    System.out.println("Please choose correct option");
-                    break;
-            }
-
-        }while(choice != 0);
-
+    static double Multiply(double a, double b) {
+    	return a * b;
+    }
+    static double Divide(double a, double b) {
+    	if (b == 0) {
+    		throw new IllegalArgumentException("Divisor cannot be zero, Exiting");
+    	}
+    	else return a / b;
     }
 }
+*/
+    static double Sqeroot(double a) {
+
+        double res = Math.sqrt(a);
+        logger.info("Square root of " + a + " is " + res);
+
+        return res;
+    }
+
+    static long Fact(long b) {
+
+        //double res = Math.sqrt(a);
+        long res=1;
+        if(b<0) {
+                System.out.println("Enter number >= 0");
+            logger.info("Number entered for Square root is negative.");
+            return -1;
+                }
+        else {
+            for (long i = b; i > 0; i--) {
+                res = res * i;
+            }
+            logger.info("Factorial of " + b + " is " + res);
+            return res;
+        }
+
+
+
+        }
+
+
+
+    static double Loge(double a) {
+
+        double res = Math.log(a);
+        logger.info("Log of " + a + "is" + res);
+
+        return res;
+    }
+    static double Powe(double a,double b) {
+
+        double res = Math.pow(a,b);
+        logger.info("Power of " + a + "and "+b+" is " + res);
+
+        return res;
+    }
+
+
+
+
+    }
+
+
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,69 +1,60 @@
-import org.junit.Test;
+package calculator;
+
 import static org.junit.Assert.*;
+
+import org.junit.*;
 
 public class CalculatorTest {
 
-    Calculator calculator = new Calculator();
-    private static final double DELTA = 1e-9;
+		private App calculator;
+	    
+	    @Before
+	    public void setUp() {
+	        calculator = new App();
+	    }
 
-    @Test
-    public void squareRoot_True(){
-        assertEquals("True: Square Root of number ", 9.0, calculator.squareRoot(81), DELTA);
-        assertEquals("True: Square Root of number ", 4.7958315233127 , calculator.squareRoot(23), DELTA);
-        assertEquals("True: Square Root of number ", Double.NaN, calculator.squareRoot(-10), DELTA);
-    }
+		@Test
+	    public void testSqeroot() {
+	        double a = 4.0;
 
-    @Test
-    public void squareRoot_False(){
-        assertNotEquals("False: Square Root of number ", 1.0, calculator.squareRoot(18), DELTA);
-        assertNotEquals("False: Square Root of number ", 1.0, calculator.squareRoot(32), DELTA);
-        assertNotEquals("False: Square Root of number ", 1.0, calculator.squareRoot(-16), DELTA);
-    }
+	        double expectedResult = 2.0;
+	        double result = calculator.Sqeroot(a);
+	        Assert.assertEquals(expectedResult, result, 0);
+	    }
+		
+		@Test
+		public void testFact() {
+			long a = 10;
 
-    @Test
-    public void factorial_True(){
-        assertEquals("True: Factorial of number for True positive", 24.0, calculator.factorial(4), DELTA);
-        assertEquals("True: Factorial of number for True positive", 720.0, calculator.factorial(6), DELTA);
-        assertEquals("True: Factorial of number for True positive", Double.NaN, calculator.factorial(-15), DELTA);
-        assertEquals("True: Factorial of number for True positive", Double.NaN, calculator.factorial(10.5123), DELTA);
-    }
+	        long expectedResult = 3628800;
+	        double result = calculator.Fact(a);
+	        Assert.assertEquals(expectedResult, result, 0);
+		}
 
-    @Test
-    public void factorial_False(){
-        assertNotEquals("False: Factorial of number for False positive", 1.0, calculator.factorial(2), DELTA);
-        assertNotEquals("False: Factorial of number for False positive", 1.0, calculator.factorial(10), DELTA);
-        assertNotEquals("False: Factorial of number for False positive", 1.0, calculator.factorial(-4), DELTA);
+	@Test
+	public void testLoge() {
+		double a = 1;
 
+		double expectedResult = 0.0;
+		double result = calculator.Loge(a);
+		Assert.assertEquals(expectedResult, result, 0.0001);
+	}
+		
+		@Test
+		public void testPowe() {
+			double a = 7;
+	        double b = 3;
+	        double expectedResult = 343;
+	        double result = calculator.Powe(a, b);
+	        Assert.assertEquals(expectedResult, result, 0);
+		}
+		
 
-    }
-
-    @Test
-    public void naturalLog_True(){
-        assertEquals("True: Factorial of number for True positive", 11.721669844, calculator.naturalLog(123213), DELTA);
-        assertEquals("True: Factorial of number for True positive", 6.67076632085, calculator.naturalLog(789), DELTA);
-        assertEquals("True: Factorial of number for True positive", Double.NaN, calculator.naturalLog(-100), DELTA);
-    }
-
-    @Test
-    public void naturalLog_False(){
-        assertNotEquals("False: Factorial of number for False positive", 1,calculator.naturalLog(21243), DELTA);
-        assertNotEquals("False: Factorial of number for False positive", 1,calculator.naturalLog(231131), DELTA);
-        assertNotEquals("False: Factorial of number for False positive", 1, calculator.naturalLog(-131123), DELTA);
-    }
-
-    @Test
-    public void mypower_True(){
-        assertEquals("True: Factorial of number for True positive", 256.0, calculator.mypower(2, 8), DELTA);
-        assertEquals("True: Factorial of number for True positive", 125,  calculator.mypower(5,3), DELTA);
-        assertEquals("True: Factorial of number for True positive", Double.NaN,  calculator.mypower(-12,42.232), DELTA);
-    }
-
-    @Test
-    public void mypower_False(){
-        assertNotEquals("False: Factorial of number for False positive", 1.0,  calculator.mypower(213, 2), DELTA);
-        assertNotEquals("False: Factorial of number for False positive", 1.0, calculator.mypower(96,86), DELTA);
-        assertNotEquals("False: Factorial of number for False positive", 1.0, calculator.mypower(-73,74.3), DELTA);
-    }
-
+		
+		/*@Test(expected = IllegalArgumentException.class)
+	    public void testDivideByZero() {
+	        int a = 25;
+	        int b = 0;
+	        calculator.Divide(a, b);
+	    }*/
 }
-
